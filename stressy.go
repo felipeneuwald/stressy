7d3c5f476bf5e39042be7e8da2c67d813f361742
Felipe Neuwald <felipebgn@gmail.com>
2020-04-07 23:08:27 +0100

    main changes

--- stressy.go
+++ stressy.go
@@ -1,91 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"os/signal"
 	"runtime"
 	"sync"
 	"time"
 
-	// "flag"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
-const parallel = 2
+var testType string
+var parallelism int
+
+func init() {
+	flag.StringVar(&testType, "type", "bcrypt", "Test type")
+	flag.IntVar(&parallelism, "parallelism", 1, "Number of parallel resource operations")
+	flag.Parse()
+}
 
 func main() {
-	// cpuTest := flag.String("cpu", "bcrypt", "type of CPU test")
-	// infinite := flag.Bool("infinite", false, "infinte?")
-	// threads := flag.Int("threads", 1, "number of threads")
-	//
-	// flag.Parse()
-	//
-	// fmt.Println(cpuTest, *cpuTest)
-	// fmt.Println(infinite, *infinite)
-	// fmt.Println(threads, *threads)
-	// fmt.Println(flag.Args())
 
-	// // it works. have to study, understand that and see why it should be used.
-	// c := make(chan os.Signal, 1)
-	// signal.Notify(c, os.Interrupt)
-	// go func() {
-	// 	for sig := range c {
-	// 		fmt.Println("^C, Bye.", sig)
-	// 		os.Exit(1)
-	// 	}
-	// }()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Println(sig)
+			bye(1, "bye bye")
+		}
+	}()
 
-	cpuBcrypt()
-	// cpuLoop()
-
-}
-
-func cpuLoop() {
-	for i := 0; i <= 1; i++ {
-		i = 0
-	}
-}
-
-func cpuBcrypt() {
-	// defer bye()
+	fmt.Println("NumGoroutine", runtime.NumGoroutine())
+	fmt.Println("NumCPU", runtime.NumCPU())
+	fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(0))
 
 	var wg sync.WaitGroup
-	wg.Add(parallel)
-
-	for p := 0; p < parallel; p++ {
+	wg.Add(parallelism)
+	for p := 0; p < parallelism; p++ {
 		go func() {
-
-			runtime.Gosched()
-
-			for i := 0; i <= 1; i++ {
-				fmt.Printf(".")
-				rand.Seed(time.Now().UnixNano())
-				i = rand.Int()
-				s := fmt.Sprintf("%v", i)
-				bs := []byte(s)
-
-				_, err := bcrypt.GenerateFromPassword(bs, bcrypt.MaxCost)
-				if err != nil {
-					fmt.Println("bcrypt.GenerateFromPassword:", err)
-					os.Exit(1)
-				}
-
-				i = 0
-				fmt.Printf("+")
+			switch testType {
+			case "bcrypt":
+				runtime.Gosched()
+				testBcrypt()
+			case "loop":
+				runtime.Gosched()
+				testLoop()
+			default:
+				fmt.Println("NumGoroutine", runtime.NumGoroutine())
+				bye(1, "Unknown test type")
 			}
 			wg.Done()
 		}()
 	}
+
+	fmt.Println(runtime.NumGoroutine())
+
 	wg.Wait()
+
 }
 
-// OLD func cpuBcrypt, no concurrency. Not in use.
-func cpuBcryptOLD() {
-	// defer bye()
+func testBcrypt() {
+	fmt.Println("bcrypt")
+	// fmt.Println(runtime.NumGoroutine())
 
 	for i := 0; i <= 1; i++ {
+		// fmt.Printf(".")
 		rand.Seed(time.Now().UnixNano())
 		i = rand.Int()
 		s := fmt.Sprintf("%v", i)
@@ -98,9 +80,19 @@ func cpuBcryptOLD() {
 		}
 
 		i = 0
+		// fmt.Printf("+")
 	}
 }
 
-// func bye() {
-// 	fmt.Println("Bye.")
-// }
+func testLoop() {
+	fmt.Println("loop")
+	i := 0
+	for {
+		i++
+	}
+}
+
+func bye(i int, s string) {
+	fmt.Println(s)
+	os.Exit(i)
+}
